e9a626dd823038462a2a65dc7bedfaaccd0fee0d
gregmurray <gd-murray@hotmail.com>
2019-05-28 13:40:22 -0400

    added a bunch of conf stuff?

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -7,8 +7,11 @@ export COMMIT_SHA1=$COMMIT_SHA1
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
-envsubst <./kube/web-deployment.yaml >./kube/web-deployment.yaml.out
-mv ./kube/web-deployment.yaml.out ./kube/web-deployment.yaml
+envsubst <./kube/django/deployment.yaml >./kube/django/deployment.yaml.out
+mv ./kube/django/deployment.yaml.out ./kube/django/deployment.yaml
+
+envsubst <./kube/django/job-migration.yaml >./kube/django/job-migration.yaml.out
+mv ./kube/django/job-migration.yaml.out ./kube/django/job-migration.yaml
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
