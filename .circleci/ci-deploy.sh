0cef9a2769fb905b7ec6433e7d10689e7e48353d
gregmurray <gd-murray@hotmail.com>
2019-05-28 19:27:30 -0400

    add psql to kube deploy

--- .circleci/ci-deploy.sh
+++ .circleci/ci-deploy.sh
@@ -25,3 +25,10 @@ echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
   --certificate-authority=cert.crt \
   --token=$KUBERNETES_TOKEN \
   apply -f ./kube/django/
+
+./kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  apply -f ./kube/postgres/
