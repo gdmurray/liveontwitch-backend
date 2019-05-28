4ec15169bf22e53b7da4fa1738fa036c9ffe303a
gregmurray <gd-murray@hotmail.com>
2019-05-28 12:48:47 -0400

    yaml not yml lol

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -7,8 +7,8 @@ export COMMIT_SHA1=$COMMIT_SHA1
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
-envsubst <./kube/web-deployment.yaml.yml >./kube/web-deployment.yml.out
-mv ./kube/web-deployment.yml.out ./kube/web-deployment.yml
+envsubst <./kube/web-deployment.yaml >./kube/web-deployment.yaml.out
+mv ./kube/web-deployment.yaml.out ./kube/web-deployment.yaml
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
