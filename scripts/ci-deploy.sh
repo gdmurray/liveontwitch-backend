025e6f39b49a92f216c277f1d03df8f77623ef85
gregmurray <gd-murray@hotmail.com>
2019-05-28 12:45:40 -0400

    fixed deploy script

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -7,8 +7,8 @@ export COMMIT_SHA1=$COMMIT_SHA1
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
-envsubst <./kube/do-sample-deployment.yml >./kube/do-sample-deployment.yml.out
-mv ./kube/do-sample-deployment.yml.out ./kube/do-sample-deployment.yml
+envsubst <./kube/web-deployment.yaml.yml >./kube/web-deployment.yml.out
+mv ./kube/web-deployment.yml.out ./kube/web-deployment.yml
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
