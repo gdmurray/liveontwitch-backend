15607812bf9504b26dd5cbc26d98f81ba446c5fb
gregmurray <gd-murray@hotmail.com>
2019-05-28 18:12:16 -0400

    removed prod db conf

--- liveontwitch/prod.py
+++ liveontwitch/prod.py
@@ -8,14 +8,3 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 CELERY_BROKER_URL = 'redis://redis:6379'
 CELERY_RESULT_BACKEND = 'redis://redis:6379'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-        'PASSWORD': 'postgres'
-    }
-}
