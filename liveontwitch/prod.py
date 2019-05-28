3c5653d34db5925084a63f159821109027238eb7
gregmurray <gd-murray@hotmail.com>
2019-05-28 18:47:12 -0400

    attempting default psql pass

--- liveontwitch/prod.py
+++ liveontwitch/prod.py
@@ -8,3 +8,14 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 CELERY_BROKER_URL = 'redis://redis:6379'
 CELERY_RESULT_BACKEND = 'redis://redis:6379'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'liveontwitchdb'),
+        'USER': os.environ.get('DB_USER', 'greg'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': 5432,
+        'PASSWORD': 'postgres'
+    }
+}
